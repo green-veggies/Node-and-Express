475a1b32a8ed492992a2ee474d5d2f5bffcbf685
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2023-10-14 01:06:53 +0530

    git update

--- node/1.intro.js
+++ node/1.intro.js
@@ -1,7 +1,9 @@
 const amount="";
+const path = __dirname;
 if(amount>=10){
     console.log(`Entered item is higher than 10`);
 }
 else{
     console.log(`Entered item is less than 10`);
 }
+console.log(`File is stored in ${path}`);
