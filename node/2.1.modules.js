e676038cd1f6894527fac9bfb8be7ada931aad4e
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2023-10-14 02:26:26 +0530

    git commit

---
+++ node/2.1.modules.js
@@ -0,0 +1,6 @@
+
+const name1 = "jasrotia"
+const name2 = "jack"
+
+module.exports = {name1,name2}
+
---
+++ node/2.modules.js
@@ -0,0 +1,8 @@
+
+const names = require("./2.1.modules")
+let sayHi = require("./2.2.modules")
+console.log(names);
+
+sayHi(`Aditya`);
+sayHi(names.name1);
+sayHi(names.name2);
