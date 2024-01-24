60467bbfd9e75169db8f65ca83202af44c937c61
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-01-25 00:08:27 +0530

    updated

--- Express/6.StaticFiles/public/app.js
+++ Express/6.StaticFiles/public/app.js
@@ -3,6 +3,5 @@ var blackOut=false;
 button.addEventListener("click", () =>{
     if(blackOut){document.body.style.backgroundColor = "";}
     else{document.body.style.backgroundColor = "grey";}
-    blackOut=!blackOut;
-    
+    blackOut=!blackOut;   
 });
